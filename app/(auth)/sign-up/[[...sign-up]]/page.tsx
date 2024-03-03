d8cf08ea012699a621bb0866c07734090929df91
Prince Narteh <prinarteh@gmail.com>
2024-03-02 21:44:00 -0800

    feat: add sign up page

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+const Page = () => {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <SignUp />
+    </div>
+  );
+};
+
+export default Page;
